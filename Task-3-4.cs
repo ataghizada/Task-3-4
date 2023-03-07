c01348fe66ce8a7fe3b9d44ff44b3a43002bf11c
Ali <alitaqizade2@gmail.com>
2023-03-07 12:57:20 +0400

    added task 4

---
+++ Task-3-4.cs
@@ -0,0 +1,43 @@
+﻿//// Task 3
+
+//int num = 64;
+//int check = 0;
+
+//for (int i = 2; i <= num; i *= 2)
+//{
+//    check = i;
+//}
+
+//if (check < num)
+//{
+//    Console.WriteLine("2-nin quvveti deyildir");
+//}
+//else
+//{
+//    Console.WriteLine("2-nin quvvetidir");
+//}
+
+
+
+// Task 4
+
+// Task 4
+
+int num = 5118;
+int first = 0;
+int second = 0;
+int save = 0;
+while (num > 0)
+{
+    first = num % 10;
+    num /= 10;
+    second = num % 10;
+    num /= 10;
+
+    if (first > second && first > save)
+    {
+        save = first;
+    }
+}
+
+Console.WriteLine(save);

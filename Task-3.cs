6cbb75c1ba67ff7f4859ab522e6cbe72210efda1
Ali <alitaqizade2@gmail.com>
2023-03-07 11:12:50 +0400

    first commit

---
+++ Task-3.cs
@@ -0,0 +1,18 @@
+﻿// Task 3
+
+int num = 64;
+int check = 0;
+
+for (int i = 2; i <= num; i *= 2)
+{
+    check = i;
+}
+
+if (check < num)
+{
+    Console.WriteLine("2-nin quvveti deyildir");
+}
+else
+{
+    Console.WriteLine("2-nin quvvetidir");
+}
